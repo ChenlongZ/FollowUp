767b03d901d3f35826ffcd38e8613f2632e43212
Chenlong Zhang <chenglong.zhang@gm.com>
2017-08-03 13:32:48 -0400

    working on fragmentation

--- app/src/main/java/com/followitupapp/followitup/login/SignUpFragment.java
+++ app/src/main/java/com/followitupapp/followitup/login/SignUpFragment.java
@@ -1,10 +1,20 @@
 package com.followitupapp.followitup.login;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by zic on 8/2/17.
  */
 
 public class SignUpFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup parent, Bundle savedInstance) {
+
+        return null;
+    }
 }
