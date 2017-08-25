0e4a611c1d9e0de58707bd9d768eb8d986ca59e3
Chenlong Zhang <chenglong.zhang@gm.com>
2017-08-25 11:56:50 -0400

    add firebase libs

---
+++ app/src/main/java/com/followitupapp/followitup/models/User.java
@@ -0,0 +1,18 @@
+package com.followitupapp.followitup.models;
+
+/**
+ * Created by zic on 8/25/17.
+ */
+
+@IngoreExtraProperties
+public class User {
+
+    public String email;
+    public String userId;
+    public String
+    public User() {}
+
+    public User(String email, String userId) {
+
+    }
+}
